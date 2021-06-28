b70c34d4776ce5bd9a5fcd7d2ae07c340f92d668
CarlosMurillo2897 <42428269+CarlosMurillo2897@users.noreply.github.com>
2021-06-28 16:51:02 -0600

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Material Angular
+import { MaterialModule } from './material.module';
+
+// Components
+import { QuestionDetailComponent } from './question/question-detail.component'
+
+// Scripts
+import { MomentModule } from 'ngx-moment';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuestionDetailComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    MomentModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { };
---
+++ src/app/material.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+
+const modules = [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+];
+
+@NgModule({
+    imports: modules,
+    exports: modules,
+})
+
+export class MaterialModule {}
