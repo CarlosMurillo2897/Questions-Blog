0483d2b22453183e6d6de18c447a143bd7370bff
GAP_Limabean <GAP_Limabean@limabean.onmicrosoft.com>
2021-06-29 13:27:38 -0600

    Router enabled & routes configured. Questions Form modified, added icon selection.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SigninScreenComponent } from './auth/signin-screen.component';
+import { SignupScreenComponent } from './auth/signup-screen.component';
+
+import { QuestionListComponent } from './question/question-list.component';
+
+const routes: Routes = [
+  { path: '', component: QuestionListComponent, pathMatch: 'full' },
+  { path: 'signin', component: SigninScreenComponent },
+  { path: 'signup', component: SignupScreenComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/question/question-form.component.ts
+++ src/app/question/question-form.component.ts
@@ -6,6 +6,15 @@ import icons  from './icons';
 @Component({
   selector: 'app-question-form',
   templateUrl: './question-form.component.html',
+  styles: [`
+    i {
+      font-size: 48px;
+    }
+
+    small {
+      display: block;
+    }
+  `],
 })
 
 export class QuestionFormComponent {
@@ -16,15 +25,19 @@ export class QuestionFormComponent {
     if(!icon.versions.font.includes(version)) {
       version = icon.versions.font.includes('plain-wordmark') ? 'plain-wordmark' : icon.versions.font[0];
     }
-    
+    if(icon.name !== 'yunohost') {
+      version += ' colored';
+    }
     return version;
   }
 
   onSubmit(form: NgForm) {
-    const { title, description } = form.value;
+    const { title, description, icon } = form.value;
     const q = new Question(
       title, 
       description,
+      new Date(),
+      icon,
     );
     form.reset();
     console.log(q);
