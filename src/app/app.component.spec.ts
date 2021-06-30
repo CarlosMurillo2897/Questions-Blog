b0201441a276b4d7d19485f425ed96c028c61169
GAP_Limabean <GAP_Limabean@limabean.onmicrosoft.com>
2021-06-29 18:23:40 -0600

    Test #1.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'platzi-overflow'`, () => {
+  it(`should have as title 'Questions-Blog'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('platzi-overflow');
+    expect(app.title).toEqual('Questions-Blog');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('platzi-overflow app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Questions-Blog app is running!');
   });
 });
